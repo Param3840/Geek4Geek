10b35e3a45eaed9b3649fe974ee949235d2e3c7f
Paramveer Kumar Singh <paramveer.22scse1010256@galgotiasuniversity.edu.in>
2025-09-11 17:42:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int findPages(int[] arr, int k) {
+        
+        if(arr.length<k) return -1;
+        // code here
+        int low=maximum(arr);
+        int high=sumOfAnArray(arr);
+        int ans=-1;
+
+        while (low<=high){
+            int mid=low+(high-low)/2;
+            int studentCount=func(arr,mid);
+            if (studentCount<=k){
+                ans=mid;
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+     public static int maximum(int[] arr){
+        int max=Integer.MIN_VALUE;
+        for (int i=0;i< arr.length;i++){
+            if (arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+    public static int sumOfAnArray(int[]arr){
+        int sum=0;
+        for (int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    public static int func(int[]arr,int pages){
+        int studnet=1;
+        int pagesStudent=0;
+
+        for (int i=0;i<arr.length;i++){
+            if (pagesStudent+arr[i]<=pages){
+                pagesStudent+=arr[i];
+            }
+            else {
+                studnet++;
+                pagesStudent=arr[i];
+            }
+        }
+        return studnet;
+    }
+}
